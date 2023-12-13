772387d7afeb9b5ff7fc511aa3b5ad5e037fc8fd
Praful Bhandari <128509560+prafulbhandari07@users.noreply.github.com>
2023-12-13 23:23:35 +0530

    test

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,7 +2,7 @@ function Header(props) {
     return (
         <header>
             <div id='navbar'>
-                <h1>Countries & their info.</h1>
+                <h1>Welcome User</h1>
                 <div id="searchBar">
                     <p>Search Countries Here👉🏻 </p>
                     <input type="text" value={props.inputText} onChange={(e) => props.setInputText(e.target.value)} />
