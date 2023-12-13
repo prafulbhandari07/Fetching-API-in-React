3c359fb922358a4ea3463ded2e016db45971e76d
Praful Bhandari <128509560+prafulbhandari07@users.noreply.github.com>
2023-12-13 23:07:16 +0530

    test

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,12 +1,13 @@
 function Card(props) {
     return (
-        <div className="card-block">
+        <div className="card-block ">
             {props.countries.map((country) =>
                 <div className="card">
                     <img src={country.flags.png} alt="" />
                     <h1>Name : {country.name.official}</h1>
                     <h2>Region: {country.region}</h2>
-                    <p>Description: {country.flags.alt}</p>
+                    <h5>Description: {country.flags.alt}</h5>
+                    <hr></hr>
                 </div>
 
             )}
